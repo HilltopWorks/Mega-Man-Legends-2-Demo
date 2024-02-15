9183e9ba2fb9649ec6d9dcd2b2fb5269196dde23
HilltopWorks <115959038+HilltopWorks@users.noreply.github.com>
2024-02-15 01:21:28 -0800

    Add files via upload

--- MMLtext.py
+++ MMLtext.py
@@ -46,6 +46,30 @@ text_files =   {"unpack\DAT\ST07\ST07-0x00012000-1.bin": 0,
                 "src\SLPS_021.09|2": 0x533E0,
                 "src\SLPS_021.09|3": 0x535D8}
 
+area_offset = 0x535D8
+
+pause_text_loads = {
+                    "upper":[0x8001b128],
+                    "lower":[0x8001b130]
+                    }
+
+button_text_loads = {
+                    "upper":[0x80041b54,0x80041be4],
+                    "lower":[0x80041b58,0x80041be8]
+                    }
+
+area_text_loads = {
+                    "upper":[0x80043030],
+                    "lower":[0x80043034]
+                    }
+
+parts_text_loads = {
+                    "upper":[0x80041990,0x800419dc,0x80043050],
+                    "lower":[0x80041994,0x800419e0,0x80043054]
+                    }
+
+exe_text_start = 0x800627b0
+exe_text_size_max = 0x190F
 
 def make_control_code_list():
     for x in range(0x38):
@@ -109,8 +133,7 @@ def extractText():
     
     return
 
-
-def injectTextToBin(text_file_path, out_folder):
+def injectTextToBin(text_file_path):
     injected_bin = {}
 
     text_file = open(text_file_path, "r", encoding="utf8")
@@ -128,12 +151,14 @@ def injectTextToBin(text_file_path, out_folder):
 
         if "SLPS" in src_path:
             file_offset = int(src_line.split("|")[1],16)
+        elif "SUBSCN" in src_path:
+            file_offset = 0x7304
         else:
             file_offset = 0
         src_file.seek(file_offset)
 
-        n_line_entries = (int.from_bytes(src_file.read(2), "little")//2) - 1
-        header_buffer += (n_line_entries*2).to_bytes(2, "little")
+        n_line_entries = (int.from_bytes(src_file.read(2), "little")//2)
+        header_buffer += ((n_line_entries)*2).to_bytes(2, "little")
 
         lines = file_text.split(line_header)[1:]
 
@@ -156,6 +181,69 @@ def injectTextToBin(text_file_path, out_folder):
 
     return injected_bin
 
+def applyLoad(address, loads, exe_file, exe_offset):
+    address_hi = address >> 16
+    address_lo = address & 0xFFFF
+    
+    for hi_addr in loads["upper"]:
+        exe_file.seek(hi_addr - exe_offset)
+        exe_file.write(address_hi.to_bytes(2, "little"))
+    for lo_addr in loads["lower"]:
+        exe_file.seek(lo_addr - exe_offset)
+        exe_file.write(address_lo.to_bytes(2, "little"))
+    return
+
+def injectAll(text_file_path, exe_path):
+    injected_bins = injectTextToBin(text_file_path)
+
+    for file_path in injected_bins:
+
+        if "DAT" in file_path:
+            #DAT file text
+            target_path = file_path.replace("unpack", "unpack_edit")
+            target_offset = text_files[file_path]
+            if "SUBSCN" in file_path:
+                target_file = open(target_path, "r+b")
+                assert len(injected_bins[file_path]) <= 0x1F3F
+            else:
+                target_file = open(target_path, "wb")
+            
+            target_file.seek(target_offset)
+            target_file.write(injected_bins[file_path])
+            target_file.close()
+    
+    exe_pause_file = injected_bins["src\SLPS_021.09|0x52fb0"]
+    if len(exe_pause_file) % 2 != 0:
+        exe_pause_file += b"\x00"
+
+    exe_buttons_file = injected_bins["src\SLPS_021.09|0x533e0"]
+    if len(exe_buttons_file) % 2 != 0:
+        exe_buttons_file += b"\x00"
+
+    exe_area_file = injected_bins["src\SLPS_021.09|0x535d8"]
+    if len(exe_area_file) % 2 != 0:
+        exe_area_file += b"\x00"
+
+    exe_parts_file = injected_bins["src\SLPS_021.09|0x5372C"]
+    if len(exe_parts_file) % 2 != 0:
+        exe_parts_file += b"\x00"
+
+
+    exe_blob = exe_pause_file + exe_buttons_file + exe_area_file + exe_parts_file
+    assert len(exe_blob) <= exe_text_size_max
+
+    exe_file = open(exe_path, "r+b")
+
+    exe_file.seek(exe_text_start - 0x8000f800)
+    exe_file.write(exe_blob)
+
+    applyLoad(exe_text_start                                                                   , pause_text_loads, exe_file, 0x8000f800)
+    applyLoad(exe_text_start + len(exe_pause_file)                                             , button_text_loads, exe_file, 0x8000f800)
+    applyLoad(exe_text_start + len(exe_pause_file) + len(exe_buttons_file)                     , area_text_loads, exe_file, 0x8000f800)
+    applyLoad(exe_text_start + len(exe_pause_file) + len(exe_buttons_file) + len(exe_area_file), parts_text_loads, exe_file, 0x8000f800)
+    return
+
+
 #make_control_code_list()
 #convertMSG("src\\DAT\\ST22.BIN", 0x18830)
 #convertMSG("unpack\DAT\ST24\ST24-0x00026800-1.bin", 0)
@@ -167,5 +255,5 @@ def injectTextToBin(text_file_path, out_folder):
 #convertRaw("PAUSE")
 
 #extractText()
-                
-injectTextToBin("recieved\\MML_text.txt", "")
+injectAll("recieved\\MML_text.txt", "src_edit\\SLPS_021.09")
+#injectTextToBin("recieved\\MML_text.txt")
